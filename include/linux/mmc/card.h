4612c7595a15bf57f1cc3290a1693d3c0620aa3e
Gary King <gking@nvidia.com>
2009-12-07 18:39:21 -0800

    mmc: add detection for MMC 4.4 boot sectors
    properly unpacks the boot sector extension in MMC 4.4 CID responses

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -17,6 +17,7 @@ struct mmc_cid {
 	unsigned int		manfid;
 	char			prod_name[8];
 	unsigned int		serial;
+	unsigned char		cbx:2;
 	unsigned short		oemid;
 	unsigned short		year;
 	unsigned char		hwrev;
@@ -306,6 +307,7 @@ static inline void __maybe_unused remove_quirk(struct mmc_card *card, int data)
 #define mmc_card_mmc(c)		((c)->type == MMC_TYPE_MMC)
 #define mmc_card_sd(c)		((c)->type == MMC_TYPE_SD)
 #define mmc_card_sdio(c)	((c)->type == MMC_TYPE_SDIO)
+#define mmc_card_has_boot(c)	((c)->cid.cbx != 0)
 
 #define mmc_card_present(c)	((c)->state & MMC_STATE_PRESENT)
 #define mmc_card_readonly(c)	((c)->state & MMC_STATE_READONLY)
