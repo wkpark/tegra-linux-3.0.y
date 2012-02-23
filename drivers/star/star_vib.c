8b838a24bc0769607a94be4044dd325814831eaf
Won-Kyu Park <wkpark@gmail.com>
2012-02-23 20:09:47 +0900

    star_vib: cleanup
    use pr_err/pr_debug, cleanup

--- drivers/star/star_vib.c
+++ drivers/star/star_vib.c
@@ -1,3 +1,18 @@
+/*
+ * linux/drivers/star/star_vib.c
+ *
+ * Copyright (C) 2010 LG Electronics, Inc.
+ * Author: sk.hwang@lge.com
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -11,17 +26,12 @@
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
 #include <linux/timer.h>
-
-//20100717 sk.hwang@lge.com For HRTIMER @linux 2.6.32[start]
 #include <linux/hrtimer.h>
-//20100717 sk.hwang@lge.com For HRTIMER @linux 2.6.32[end]
 
 #include "nvcommon.h"
 #include "nvodm_services.h"
 #include "nvodm_query.h"
 #include "nvodm_query_discovery.h"
-
-#define VIB_DEBUG 0 
 
 typedef struct star_vib_device_data
 {
@@ -29,108 +39,78 @@ typedef struct star_vib_device_data
 	NvOdmGpioPinHandle  h_vib_gpio_pin;
 	NvOdmServicesPmuHandle  h_vib_pmu;
 	struct hrtimer timer;
-	
+
 	NvU32   vdd_id;
-    NvU32   en_pin;
-    NvU32   en_port;
+	NvU32   en_pin;
+	NvU32   en_port;
 	struct delayed_work delayed_work_vib;
-}star_vib_device;
+} star_vib_device;
 
 static star_vib_device *g_vib;
 static NvBool vib_enable = false;
-static volatile int vib_stay_time = 0; 
+static volatile int vib_stay_time = 0;
 static atomic_t is_enable = ATOMIC_INIT(0);
 //static struct timer_list vib_timer;
 
-
+#if 0
 static NvU32 toggle = 0;
 static void star_vib_work_func(struct work_struct *work)
 {
 	NvU32 gpio_state = 0;	
-	#if VIB_DEBUG 
-	printk("vibrator test... value = %d\n", toggle);
-	#endif
+	pr_debug("vibrator test... value = %d\n", toggle);
 
+	NvOdmGpioConfig(g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, NvOdmGpioPinMode_Output);
 	
-	NvOdmGpioConfig( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, NvOdmGpioPinMode_Output);
-	
-	NvOdmGpioSetState( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, toggle );
-	if( toggle == 0 )
+	NvOdmGpioSetState(g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, toggle);
+	if (toggle == 0)
 		toggle++;
 	else	
 		toggle = 0;
 
-
 	NvOdmGpioGetState(g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, &gpio_state); 	
-	#if VIB_DEBUG
-	printk("[skhwang] VIB_EN = %d\n", gpio_state);
-	#endif
+	pr_debug("[skhwang] VIB_EN = %d\n", gpio_state);
 
 	NvOdmOsSleepMS(1000);
 	schedule_delayed_work(&g_vib->delayed_work_vib,150);
-	
 }
-
+#endif
 
 // 20100903 taewan.kim@lge.com Power control bug fix [START]
-static int star_vib_set_power_rail( NvU32 vdd_id, NvBool is_enable )
+static int star_vib_set_power_rail(NvU32 vdd_id, NvBool is_enable)
 {
-    NvOdmServicesPmuHandle h_pmu = NvOdmServicesPmuOpen();
-    NvOdmServicesPmuVddRailCapabilities vddrailcap;
-    NvU32 settletime;
+	NvOdmServicesPmuHandle h_pmu = NvOdmServicesPmuOpen();
+	NvOdmServicesPmuVddRailCapabilities vddrailcap;
+	NvU32 settletime;
 
-    if(h_pmu)
-    {
-        NvOdmServicesPmuGetCapabilities( h_pmu, vdd_id, &vddrailcap );
-        if( is_enable )
-        {
-#if VIB_DEBUG
-            printk("[skhwang] vibrator PMU enable\n");
-#endif
-            NvOdmServicesPmuSetVoltage(h_pmu, vdd_id, vddrailcap.requestMilliVolts, &settletime);
-        }
-        else
-        {
-#if VIB_DEBUG
-            printk("[skhwang] vibrator PMU do not enable\n");
-#endif
-            NvOdmServicesPmuSetVoltage(h_pmu, vdd_id, NVODM_VOLTAGE_OFF, &settletime);
-        }
+	if (h_pmu) {
+		NvOdmServicesPmuGetCapabilities(h_pmu, vdd_id, &vddrailcap);
+		if (is_enable) {
+			pr_debug("%s: vibrator PMU enable\n", __func__);
+			pr_info("%s: Max millivolt=%d\n", __func__, vddrailcap.requestMilliVolts);
+			NvOdmServicesPmuSetVoltage(h_pmu, vdd_id, vddrailcap.requestMilliVolts, &settletime);
+		} else {
+			pr_debug("[skhwang] vibrator PMU do not enable\n");
+			NvOdmServicesPmuSetVoltage(h_pmu, vdd_id, NVODM_VOLTAGE_OFF, &settletime);
+		}
 
-        if(settletime)
-            NvOdmOsWaitUS(settletime);
+		if (settletime)
+			NvOdmOsWaitUS(settletime);
 
-        NvOdmServicesPmuClose(h_pmu);
-#if VIB_DEBUG
-        printk("[skhwang] vibrator voltage =  %d or %d \n", vddrailcap.requestMilliVolts, vddrailcap.MinMilliVolts);
-#endif
+		NvOdmServicesPmuClose(h_pmu);
+		pr_debug("[skhwang] vibrator voltage = %d or %d\n",
+				vddrailcap.requestMilliVolts, vddrailcap.MinMilliVolts);
 
-        return 0;
-    }
+		return 0;
+	}
 
-    return -1;
+	return -1;
 }
 // 20100903 taewan.kim@lge.com Power control bug fix [END]
 
-static void star_vib_vibrating( NvBool on )
+static void star_vib_vibrating(NvBool on)
 {
-//20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
-#ifdef CONFIG_MACH_STAR_SKT_REV_E
-	NvBool vib_onoff;
-	
-	if(on)
-		vib_onoff = true;
-	else
-		vib_onoff = false;
-
 	NvOdmGpioConfig( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, NvOdmGpioPinMode_Output);
-	NvOdmGpioSetState( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, vib_onoff );
-//20101112 sh80.choi@lge.com Call Vibrate Volume [END_LGE_LAB1]
-#else
-
-	NvOdmGpioConfig( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, NvOdmGpioPinMode_Output);
-	NvOdmGpioSetState( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, on );
-#endif
+	NvOdmGpioSetState( g_vib->h_vib_gpio, g_vib->h_vib_gpio_pin, !!on);
 }
 
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
@@ -139,50 +119,46 @@ NvOdmServicesPwmHandle hOdmPwm = NULL;
 
 NvBool star_vibrator_pwn_set(NvBool IsEnable)
 {
-    NvU32 RequestedPeriod, ReturnedPeriod;
+	NvU32 RequestedPeriod, ReturnedPeriod;
 	NvU32 gain;
-    RequestedPeriod = 22930; 
+	RequestedPeriod = 22930;
 
 	gain = IsEnable << 16;
-	
-    if (!hOdmPwm) {
-        pr_err("%s: failed to open NvOdmPwmOpen\n", __func__);
-        return NV_FALSE;
-    }
-	
+
+	if (!hOdmPwm) {
+		pr_err("%s: failed to open NvOdmPwmOpen\n", __func__);
+		return NV_FALSE;
+	}
+
 	if (IsEnable)
 	{		
-		printk("[%s] Start Enable...duty[0x%x]\n", __func__, gain);
+		pr_debug("[%s] Start Enable...duty[0x%x]\n", __func__, gain);
 		NvOdmPwmConfig(hOdmPwm, NvOdmPwmOutputId_PWM3, NvOdmPwmMode_Enable, gain, &RequestedPeriod, &ReturnedPeriod);
 	}
 	else
 	{
-		printk("[%s] Start Disable...duty[0x%x]\n", __func__, gain);
+		pr_debug("[%s] Start Disable...duty[0x%x]\n", __func__, gain);
 		NvOdmPwmConfig(hOdmPwm, NvOdmPwmOutputId_PWM3, NvOdmPwmMode_Disable, 0, &RequestedPeriod, &ReturnedPeriod);
 	}
-	
+
 	return NV_TRUE;
-	   
 }
 #endif
 //20101112 sh80.choi@lge.com Call Vibrate Volume [END_LGE_LAB1]
 
-static void star_vib_enable( NvBool is_enable )
+static void star_vib_enable(NvBool is_enable)
 {
-	if( is_enable)
-	{
+	if( is_enable) {
         //star_vib_set_power_rail( g_vib->h_vib_pmu, g_vib->vdd_id, is_enable);
-		star_vib_vibrating( is_enable );
+		star_vib_vibrating(is_enable);
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifdef CONFIG_MACH_STAR_SKT_REV_E
 		star_vibrator_pwn_set(is_enable);
 #endif
 //20101112 sh80.choi@lge.com Call Vibrate Volume [END_LGE_LAB1]
-	}
-	else
-	{
+	} else {
         //star_vib_set_power_rail( g_vib->h_vib_pmu, g_vib->vdd_id, is_enable);
-		star_vib_vibrating( is_enable );
+		star_vib_vibrating(is_enable);
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifdef CONFIG_MACH_STAR_SKT_REV_E
 		star_vibrator_pwn_set(is_enable);
@@ -198,12 +174,10 @@ static void star_vib_timeout(unsigned long arg )
 	del_timer(&vib_timer);
 }
 */
-static enum hrtimer_restart star_vib_timeout(struct hrtimer *timer )
+static enum hrtimer_restart star_vib_timeout(struct hrtimer *timer)
 {
-	#if VIB_DEBUG
-	printk("[%s] enter timer function...\n", __func__);
-	printk("vibrator off...\n");
-	#endif
+	pr_debug("[%s] enter timer function...\n", __func__);
+	pr_debug("vibrator off...\n");
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]	
 #ifdef CONFIG_MACH_STAR_SKT_REV_E
 	star_vib_vibrating(NV_FALSE);
@@ -215,6 +189,7 @@ static enum hrtimer_restart star_vib_timeout(struct hrtimer *timer )
 
 	return HRTIMER_NORESTART;
 }
+
 static ssize_t star_vib_stay_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	int time = vib_stay_time;
@@ -225,30 +200,25 @@ static ssize_t star_vib_stay_store(struct device *dev, struct device_attribute *
 {
 	int value;
 	unsigned long timeout;
-	sscanf(buf, "%ld", &value );
-	#if VIB_DEBUG
-	printk("%s: Timeout value = %ld ms\n", __func__, value );
-	#endif
+
+	sscanf(buf, "%ld", &value);
+	pr_debug("%s: Timeout value = %ld ms\n", __func__, value);
 	vib_stay_time = value;
 	atomic_set(&is_enable, 1);
-	if( atomic_read(&is_enable) == 1 )
-	{
-		#if 0 
+	if (atomic_read(&is_enable) == 1) {
+#if 0
 		timeout = (unsigned long)(value * 1000 * 1000 );
 		hrtimer_start(&g_vib->timer, ktime_set(0, timeout), HRTIMER_MODE_ABS);
-		#else
-		if( value < 1000 )
-		{
-			timeout = (unsigned long)(value * 1000 * 1000 );
+#else
+		if (value < 1000) {
+			timeout = (unsigned long)(value * 1000 * 1000);
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifndef CONFIG_MACH_STAR_SKT_REV_E
 //20101112 sh80.choi@lge.com Call Vibrate Volume [END_LGE_LAB1]
 			star_vib_enable(NV_TRUE);
 #endif
 			hrtimer_start(&g_vib->timer, ktime_set(0, timeout), HRTIMER_MODE_REL);
-		}
-		else
-		{
+		} else {
 			timeout = (long)value / 1000;
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifndef CONFIG_MACH_STAR_SKT_REV_E
@@ -257,7 +227,7 @@ static ssize_t star_vib_stay_store(struct device *dev, struct device_attribute *
 #endif
 			hrtimer_start(&g_vib->timer, ktime_set((long)timeout, 0), HRTIMER_MODE_REL);
 		}
-		#endif
+#endif
 	}
 
 	return count;
@@ -273,16 +243,13 @@ static ssize_t star_vib_onoff_show(struct device *dev, struct device_attribute *
 static ssize_t star_vib_onoff_store(struct device *dev, struct device_attribute *attr, char *buf, size_t count)
 {
 	u32 val = 0;
-	unsigned int timeout = 0;
+//	unsigned int timeout = 0;
 	val = simple_strtoul(buf, NULL, 10);
-	
+
 //	timeout = vib_stay_time;
 //	spin_lock(&vib_lock);
-	if(val)
-	{
-		#if VIB_DEBUG
-		printk("vibrator on...\n");	
-		#endif
+	if (val) {
+		pr_debug("vibrator on...\n");
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifdef CONFIG_MACH_STAR_SKT_REV_E
 		star_vib_enable(val);
@@ -290,11 +257,9 @@ static ssize_t star_vib_onoff_store(struct device *dev, struct device_attribute 
 //20101112 sh80.choi@lge.com Call Vibrate Volume [END_LGE_LAB1]
 		
 //		star_vib_enable(NV_TRUE);
-		#if VIB_DEBUG
-//		printk("start timer...\n");
-//		printk("app's timeout = %d\n", vib_stay_time);//timeout);
-		printk("Vibrator enabled!!!\n");
-		#endif
+//		pr_debug("start timer...\n");
+//		pr_debug("app's timeout = %d\n", vib_stay_time);//timeout);
+		pr_debug("Vibrator enabled!!!\n");
 		atomic_set(&is_enable, 1);
 	/*
 		if( timeout == 1 )
@@ -327,12 +292,8 @@ static ssize_t star_vib_onoff_store(struct device *dev, struct device_attribute 
 			hrtimer_start(&g_vib->timer, ktime_set(0, timeout*1000000), HRTIMER_MODE_REL);
 		}
 	*/		
-	}
-	else
-	{
-		#if VIB_DEBUG
-		printk("vibrator off...\n");
-		#endif
+	} else {
+		pr_debug("vibrator off...\n");
 		atomic_set(&is_enable, 0);
 		star_vib_enable(NV_FALSE);
 	}
@@ -353,7 +314,7 @@ static const struct attribute_group star_vib_group = {
 	.attrs = star_vib_attributes,
 };
 
-static int __devinit star_vib_probe(struct platform_device *pdev )
+static int __devinit star_vib_probe(struct platform_device *pdev)
 {
 	const NvOdmPeripheralConnectivity *pcon;
 	int err;
@@ -361,13 +322,12 @@ static int __devinit star_vib_probe(struct platform_device *pdev )
 	int loop;
 	struct device *dev = &pdev->dev;
 
-	printk("[%s] probing vibrator\n",__func__);
+	pr_debug("[%s] probing vibrator\n",__func__);
 
-	g_vib = kzalloc(sizeof(*g_vib), GFP_KERNEL );
-	if( g_vib == NULL)
-	{
+	g_vib = kzalloc(sizeof(*g_vib), GFP_KERNEL);
+	if (g_vib == NULL) {
 		err = -1;
-		printk("[%s] fail vib\n", __func__ );
+		pr_debug("[%s] fail vib\n", __func__);
 		return err;
 	}
 
@@ -378,51 +338,40 @@ static int __devinit star_vib_probe(struct platform_device *pdev )
         // 20100903 taewan.kim@lge.com Power control bug fix [END]
 
 	pcon = (NvOdmPeripheralConnectivity*)NvOdmPeripheralGetGuid(NV_ODM_GUID('v','i','b','r','a','t','o','r'));
-	for(loop = 0; loop< pcon->NumAddress; loop++)
-    {
-        switch(pcon->AddressList[loop].Interface)
-        {
-            case NvOdmIoModule_Gpio:
-                g_vib->en_port = pcon->AddressList[loop].Instance;
-                g_vib->en_pin = pcon->AddressList[loop].Address;
-                found_gpio = NV_TRUE;
-                break;
-            case NvOdmIoModule_Vdd:
-                {
-                    g_vib->vdd_id = pcon->AddressList[loop].Address;
-				#if VIB_DEBUG
-                    printk("VIB POWER %d\n", g_vib->vdd_id );
-				#endif
-                    if (star_vib_set_power_rail( g_vib->vdd_id, NV_TRUE) != 0)
-		        return -ENOSYS;
-                }
-                break;
-            default:
-                break;
-        }
-    }
+	for (loop = 0; loop< pcon->NumAddress; loop++) {
+		switch(pcon->AddressList[loop].Interface) {
+		case NvOdmIoModule_Gpio:
+			g_vib->en_port = pcon->AddressList[loop].Instance;
+			g_vib->en_pin = pcon->AddressList[loop].Address;
+			found_gpio = NV_TRUE;
+			break;
+		case NvOdmIoModule_Vdd:
+			g_vib->vdd_id = pcon->AddressList[loop].Address;
+			pr_debug("VIB POWER %d\n", g_vib->vdd_id);
+			if (star_vib_set_power_rail( g_vib->vdd_id, NV_TRUE) != 0)
+				return -ENOSYS;
+			break;
+		default:
+			break;
+		}
+	}
 
-	#if VIB_DEBUG
-	printk("[skhwang][%s] : vibrator Int Port = %c, Int Pin = %d\n", __func__, (g_vib->en_port+'a'), g_vib->en_pin);
-	printk("vibrator.....\n");
-	#endif
-
+	pr_debug("[skhwang][%s] : vibrator Int Port = %c, Int Pin = %d\n", __func__, (g_vib->en_port+'a'), g_vib->en_pin);
+	pr_debug("vibrator.....\n");
 
 	g_vib->h_vib_gpio = NvOdmGpioOpen();
-    if(!g_vib->h_vib_gpio )
-    {
-        printk("[skhwang][%s] : Failed to open gpio\n",__func__);
-        err = - ENOSYS;
-        return err;
-    }
+	if (!g_vib->h_vib_gpio) {
+		pr_err("[skhwang][%s] : Failed to open gpio\n",__func__);
+		err = - ENOSYS;
+		return err;
+	}
 
-    g_vib->h_vib_gpio_pin = NvOdmGpioAcquirePinHandle( g_vib->h_vib_gpio, g_vib->en_port, g_vib->en_pin );
-    if(!g_vib->h_vib_gpio_pin)
-    {
-        printk("[skhwang][%s] : Failed to acquire the pin handle\n",__func__);
-        err = -ENOSYS;
-        return err;
-    }
+	g_vib->h_vib_gpio_pin = NvOdmGpioAcquirePinHandle(g_vib->h_vib_gpio, g_vib->en_port, g_vib->en_pin);
+	if (!g_vib->h_vib_gpio_pin) {
+		pr_err("[skhwang][%s] : Failed to acquire the pin handle\n",__func__);
+		err = -ENOSYS;
+		return err;
+	}
 
 //20101112 sh80.choi@lge.com Call Vibrate Volume [START_LGE_LAB1]
 #ifdef CONFIG_MACH_STAR_SKT_REV_E
@@ -436,14 +385,11 @@ static int __devinit star_vib_probe(struct platform_device *pdev )
 	hrtimer_init(&g_vib->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	g_vib->timer.function = star_vib_timeout;
 	
-	#if VIB_DEBUG
-	printk("[vib] sysfs_create_group before....\n");
-	#endif
+	pr_debug("[vib] sysfs_create_group before....\n");
 
 	//create sys filesystem.
-	if( sysfs_create_group(&dev->kobj, &star_vib_group))
-	{
-		printk("[star vib] Failed to create sys filesystem\n");
+	if (sysfs_create_group(&dev->kobj, &star_vib_group)) {
+		pr_err("[star vib] Failed to create sys filesystem\n");
 		err = -ENOSYS;
 		return err;
 	}
@@ -456,7 +402,7 @@ int star_vib_suspend(struct platform_device *dev, pm_message_t state)
 	//printk("[SLEEP] %s start \n", __func__);
 	star_vib_set_power_rail( g_vib->vdd_id, NV_FALSE);
 
-    return 0;
+	return 0;
 }
 
 int star_vib_resume(struct platform_device *dev)
@@ -464,7 +410,7 @@ int star_vib_resume(struct platform_device *dev)
 	//printk("[SLEEP] %s start \n", __func__);
 	star_vib_set_power_rail( g_vib->vdd_id, NV_TRUE);
 
-    return 0;
+	return 0;
 }
 
 
@@ -474,24 +420,24 @@ static int __devexit star_vib_remove( struct platform_device *pdev)
 }
 
 static struct platform_driver star_vib_driver = {
-    .probe = star_vib_probe,
-    .remove = __devexit_p(star_vib_remove),
-    .suspend = star_vib_suspend,
-    .resume = star_vib_resume,    
-    .driver = {
-        .name = "star_vib_name",
-        .owner = THIS_MODULE,
-    },
+	.probe = star_vib_probe,
+	.remove = __devexit_p(star_vib_remove),
+	.suspend = star_vib_suspend,
+	.resume = star_vib_resume,
+	.driver = {
+		.name = "star_vib_name",
+		.owner = THIS_MODULE,
+	},
 };
 
 static int __init star_vib_init(void)
 {
-    return platform_driver_register(&star_vib_driver);
+	return platform_driver_register(&star_vib_driver);
 }
 
 static void __exit star_vib_exit(void)
 {
-    platform_driver_unregister(&star_vib_driver);
+	platform_driver_unregister(&star_vib_driver);
 }
 
 module_init(star_vib_init);
