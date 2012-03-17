CyanogenMod/lge-kernel-star

[tegra mmc] Generate mmcblk names based on controller.
Generate mmcblk[%d] names based on sdhci controller instance numbers.
Bug 700011

Change-Id: I2be0c88f45cb2044306b1f8b8fe98ee95a800e0e
Reviewed-on: http://git-master.nvidia.com/r/4274
Reviewed-by: Jonathan Mayo <jmayo@nvidia.com>
Tested-by: Krishna Reddy <vdumpa@nvidia.com>
Reviewed-by: Gary King <gking@nvidia.com>

drivers/mmc/card/block.c
@@ -516,11 +516,30 @@ static struct mmc_blk_data *mmc_blk_alloc(struct mmc_card *card)
 {
 	struct mmc_blk_data *md;
 	int devidx, ret;
+#ifdef CONFIG_MMC_BLOCK_DEVICE_NUMBERING
+	int idx;
+	const char *mmc_blk_name;
+#endif
 
 	devidx = find_first_zero_bit(dev_use, MMC_NUM_MINORS);
 	if (devidx >= MMC_NUM_MINORS)
 		return ERR_PTR(-ENOSPC);
 	__set_bit(devidx, dev_use);
+
+#ifdef CONFIG_MMC_BLOCK_DEVICE_NUMBERING
+	mmc_blk_name = kobject_name(&card->host->parent->kobj);
+	mmc_blk_name = mmc_blk_name ? strrchr(mmc_blk_name, '.') : mmc_blk_name;
+	if (mmc_blk_name) {
+		mmc_blk_name++;
+		idx = simple_strtol(mmc_blk_name, NULL, 10);
+		if ( (idx >= 0) && (idx != devidx) &&
+			(!test_bit(idx, dev_use)) && (idx < MMC_NUM_MINORS) ) {
+			__set_bit(idx, dev_use);
+			__clear_bit(devidx, dev_use);
+			devidx = idx;
+		}
+	}
+#endif
 
 	md = kzalloc(sizeof(struct mmc_blk_data), GFP_KERNEL);
 	if (!md) {

wkpark/tegra-linux-3.0.y

drivers/mmc/card/block.c
@@ -1135,11 +1135,30 @@ static struct mmc_blk_data *mmc_blk_alloc_req(struct mmc_card *card,
 {
 	struct mmc_blk_data *md;
 	int devidx, ret;
+#ifdef CONFIG_MMC_BLOCK_DEVICE_NUMBERING
+	int idx;
+	const char *mmc_blk_name;
+#endif
 
 	devidx = find_first_zero_bit(dev_use, max_devices);
 	if (devidx >= max_devices)
 		return ERR_PTR(-ENOSPC);
 	__set_bit(devidx, dev_use);
+
+#ifdef CONFIG_MMC_BLOCK_DEVICE_NUMBERING
+	mmc_blk_name = kobject_name(&card->host->parent->kobj);
+	mmc_blk_name = mmc_blk_name ? strrchr(mmc_blk_name, '.') : mmc_blk_name;
+	if (mmc_blk_name) {
+		mmc_blk_name++;
+		idx = simple_strtol(mmc_blk_name, NULL, 10);
+		if ( (idx >= 0) && (idx != devidx) &&
+			(!test_bit(idx, dev_use)) && (idx < max_devices) ) {
+			__set_bit(idx, dev_use);
+			__clear_bit(devidx, dev_use);
+			devidx = idx;
+		}
+	}
+#endif
 
 	md = kzalloc(sizeof(struct mmc_blk_data), GFP_KERNEL);
 	if (!md) {