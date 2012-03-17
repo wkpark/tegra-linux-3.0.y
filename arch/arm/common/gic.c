CyanogenMod/lge-kernel-star

[ARM] gic: add gic_dist_restore, gic_dist_exit routines
on systems with idle states which power-gate the logic including
the gic, such as tegra, the gic distributor needs to be shut down
and restored on entry and exit from the architecture idle code

Change-Id: I9367ec9866d40757008e1c4b3ef1aba25858bce7

arch/arm/common/gic.c
@@ -175,20 +175,14 @@ void __init gic_cascade_irq(unsigned int gic_nr, unsigned int irq)
 	set_irq_chained_handler(irq, gic_handle_cascade_irq);
 }
 
-void __init gic_dist_init(unsigned int gic_nr, void __iomem *base,
-			  unsigned int irq_start)
+static unsigned int _gic_dist_init(unsigned int gic_nr)
 {
 	unsigned int max_irq, i;
+	void __iomem *base = gic_data[gic_nr].dist_base;
 	u32 cpumask = 1 << smp_processor_id();
-
-	if (gic_nr >= MAX_GIC_NR)
-		BUG();
 
 	cpumask |= cpumask << 8;
 	cpumask |= cpumask << 16;
-
-	gic_data[gic_nr].dist_base = base;
-	gic_data[gic_nr].irq_offset = (irq_start - 1) & ~31;
 
 	writel(0, base + GIC_DIST_CTRL);
 
@@ -230,6 +224,49 @@ void __init gic_dist_init(unsigned int gic_nr, void __iomem *base,
 	for (i = 0; i < max_irq; i += 32)
 		writel(0xffffffff, base + GIC_DIST_ENABLE_CLEAR + i * 4 / 32);
 
+	return max_irq;
+}
+
+#ifdef CONFIG_PM
+void gic_dist_restore(unsigned int gic_nr)
+{
+	unsigned int max_irq, i;
+
+	if (gic_nr >= MAX_GIC_NR)
+		BUG();
+
+	max_irq = _gic_dist_init(gic_nr);
+
+	writel(1, gic_data[gic_nr].dist_base + GIC_DIST_CTRL);
+	writel(0xf0, gic_data[gic_nr].cpu_base + GIC_CPU_PRIMASK);
+	writel(1, gic_data[gic_nr].cpu_base + GIC_CPU_CTRL);
+
+	/* unmask all enabled IRQs, to restore the system to a sane state */
+	for (i = 0; i < max_irq; i++) {
+		if (get_irq_chip_data(i)==&gic_data[gic_nr]) {
+			struct irq_desc *desc = irq_to_desc(i);
+			if (desc && !(desc->status & IRQ_DISABLED))
+				gic_unmask_irq(i);
+		}
+	}
+
+}
+#endif
+
+
+void __init gic_dist_init(unsigned int gic_nr, void __iomem *base,
+			  unsigned int irq_start)
+{
+	unsigned int max_irq, i;
+
+	if (gic_nr >= MAX_GIC_NR)
+		BUG();
+
+	gic_data[gic_nr].dist_base = base;
+	gic_data[gic_nr].irq_offset = (irq_start - 1) & ~31;
+
+	max_irq = _gic_dist_init(gic_nr);
+
 	/*
 	 * Setup the Linux IRQ subsystem.
 	 */
@@ -241,6 +278,14 @@ void __init gic_dist_init(unsigned int gic_nr, void __iomem *base,
 	}
 
 	writel(1, base + GIC_DIST_CTRL);
+}
+
+void gic_dist_exit(unsigned int gic_nr)
+{
+	if (gic_nr >= MAX_GIC_NR)
+		BUG();
+
+	writel(0, gic_data[gic_nr].dist_base + GIC_DIST_CTRL);
 }
 
 void __cpuinit gic_cpu_init(unsigned int gic_nr, void __iomem *base)

wkpark/tegra-linux-3.0.y

Conflicts:
	arch/arm/common/gic.c
	arch/arm/include/asm/hardware/gic.h

arch/arm/common/gic.c
@@ -262,10 +262,9 @@ void __init gic_cascade_irq(unsigned int gic_nr, unsigned int irq)
 	irq_set_chained_handler(irq, gic_handle_cascade_irq);
 }
 
-static void __init gic_dist_init(struct gic_chip_data *gic,
-	unsigned int irq_start)
+static unsigned int _gic_dist_init(struct gic_chip_data *gic)
 {
-	unsigned int gic_irqs, irq_limit, i;
+	unsigned int gic_irqs, i;
 	void __iomem *base = gic->dist_base;
 	u32 cpumask = 1 << smp_processor_id();
 
@@ -308,6 +307,52 @@ static void __init gic_dist_init(struct gic_chip_data *gic,
 	for (i = 32; i < gic_irqs; i += 32)
 		writel_relaxed(0xffffffff, base + GIC_DIST_ENABLE_CLEAR + i * 4 / 32);
 
+	return gic_irqs;
+}
+
+#ifdef CONFIG_PM
+void gic_dist_restore(unsigned int gic_nr)
+{
+	unsigned int gic_irqs, irq_limit, i;
+	struct gic_chip_data *gic;
+
+	BUG_ON(gic_nr >= MAX_GIC_NR);
+
+	gic = &gic_data[gic_nr];
+
+	gic_irqs = _gic_dist_init(gic);
+	/*
+	 * Limit number of interrupts registered to the platform maximum
+	 */
+	irq_limit = gic->irq_offset + gic_irqs;
+	if (WARN_ON(irq_limit > NR_IRQS))
+		irq_limit = NR_IRQS;
+
+	writel_relaxed(1, gic_data[gic_nr].dist_base + GIC_DIST_CTRL);
+	writel_relaxed(0xf0, gic_data[gic_nr].cpu_base + GIC_CPU_PRIMASK);
+	writel_relaxed(1, gic_data[gic_nr].cpu_base + GIC_CPU_CTRL);
+
+	/* unmask all enabled IRQs, to restore the system to a sane state */
+	for (i = 0; i < irq_limit; i++) {
+		if (irq_get_chip_data(i)==&gic_data[gic_nr]) {
+			struct irq_desc *desc = irq_to_desc(i);
+			if (desc && !irqd_irq_disabled(&desc->irq_data))
+				gic_unmask_irq(&desc->irq_data);
+		}
+	}
+
+}
+#endif
+
+
+static void __init gic_dist_init(struct gic_chip_data *gic,
+	unsigned int irq_start)
+{
+	unsigned int gic_irqs, irq_limit, i;
+	void __iomem *base = gic->dist_base;
+
+	gic_irqs = _gic_dist_init(gic);
+
 	/*
 	 * Limit number of interrupts registered to the platform maximum
 	 */
@@ -325,6 +370,14 @@ static void __init gic_dist_init(struct gic_chip_data *gic,
 	}
 
 	writel_relaxed(1, base + GIC_DIST_CTRL);
+}
+
+void gic_dist_exit(unsigned int gic_nr)
+{
+	if (gic_nr >= MAX_GIC_NR)
+		BUG();
+
+	writel(0, gic_data[gic_nr].dist_base + GIC_DIST_CTRL);
 }
 
 static void __cpuinit gic_cpu_init(struct gic_chip_data *gic)